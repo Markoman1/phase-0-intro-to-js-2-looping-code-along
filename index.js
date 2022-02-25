1cd8afb9f780362c426a05a38984f83a1b56af09
Mark <mgalex12@icloud.com>
2022-02-25 15:29:52 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+// const cards = [];
+
+// function writeCards(namesArray, string) {
+//     console.log(namesArray,string)
+//     for (let i = 0; i < namesArray.length; i++) {
+//         console.log(`Thank you, ${namesArray[i]} for the wonderful birthday gift!`)
+//         cards.push(`Thank you, ${namesArray[i]} for the wonderful birthday gift!`)
+//     }  
+//     console.log(cards)
+//     return cards;
+// }
+// writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+
+
+function countDown(i) {
+    while (i>=0) {
+        console.log(i)
+        i--
+    }
+}
+countDown(10)
